30965584c0756cd32cbe1b9c63f576c66387afba
Miklós Fazekas <mfazekas@szemafor.com>
2023-06-18 18:25:48 +0200

    get issue number

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello Wold!")
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { ESLint } = require('eslint');
+
+function log(...args: any[]) {
+  console.log('=>', ...args);
+}
+
+async function run() {
+  try {
+    const issueNumber = getIssueNumber();
+    log("issueNumber", issueNumber);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+function getIssueNumber() {
+  const { issue } = github.context.payload;
+  if (!issue) {
+    throw new Error('Could not find issue in context');
+  }
+  return issue.number;
+}
+
+log("Hello Wold!")
+run();
