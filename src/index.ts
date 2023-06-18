8011bc14ee820b6a0b884612f74a3e966df1cbb6
Miklós Fazekas <mfazekas@szemafor.com>
2023-06-18 19:21:32 +0200

    extract code

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,32 @@ async function run() {
   try {
     const issueNumber = getIssueNumber();
     log("issueNumber", issueNumber);
+    const [code, { isTypescript = false }] = getCode();
+    log("code", code, "isTypescript", isTypescript);
   } catch (error: any) {
     core.setFailed(error.message);
   }
+}
+
+function getCode():[string | undefined, { isTypescript?: boolean }] {
+  const { issue } = github.context.payload;
+  if (!issue) {
+    throw new Error('Could not find issue in context');
+  }
+  const { body } = issue;
+  const start = body.search(/```(jsx?|tsx?|javascript|typescript)/i);
+  if (start < 0) {
+    return [undefined, { isTypescript: undefined }];
+  }
+  const end = body.indexOf('```', start + 1);
+  const bodywithprefix = body.substring(start, end);
+
+  const isTypescript = !!bodywithprefix.match(/```(tsx?|typescript)/i);
+
+  return [
+    bodywithprefix.replace(/^```(jsx?|tsx?|javascript|typescript)/i, ''),
+    { isTypescript },
+  ];
 }
 
 function getIssueNumber() {
